17af611f9ac6d30996a8d2aa00b85b0bd0acfc2e
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-01-05 03:00:53 -0500

    shift method

---
+++ leetcode-easy/LinkedList/LinkedList_shift.js
@@ -0,0 +1,51 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+    let temp = this.head;
+    this.head = this.head.next;
+    temp.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return temp;
+  }
+}
+
+const myLinkedList = new LinkedList(1);
+
+myLinkedList.push(2);
+myLinkedList.push(3);
+
+myLinkedList.shift();
+
+console.log(myLinkedList);
