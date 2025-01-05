4e67b1e934937d1d211b98b53cced08f2afff557
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-01-05 03:07:30 -0500

    set method

---
+++ leetcode-easy/LinkedList/LinkedList_set.js
@@ -0,0 +1,58 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      return undefined;
+    }
+    let temp = this.head;
+    for (let i = 0; i < index; i++) {
+      temp = temp.next;
+    }
+    return temp;
+  }
+
+  set(index, value) {
+    const temp = this.get(index);
+    if (temp) {
+      temp.value = value;
+      return true;
+    }
+    return false;
+  }
+}
+
+const myLinkedList = new LinkedList(1);
+
+myLinkedList.push(2);
+myLinkedList.push(3);
+
+const val = myLinkedList.set(2, 10);
+
+console.log(val);
+console.log(myLinkedList);
