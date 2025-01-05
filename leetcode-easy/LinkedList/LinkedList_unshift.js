677df14c3955b5d11f65b379f7391d14e7cfc317
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-01-05 02:50:29 -0500

    unshift method

---
+++ leetcode-easy/LinkedList/LinkedList_unshift.js
@@ -0,0 +1,50 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+const myLinkedList = new LinkedList(1);
+
+myLinkedList.push(2);
+myLinkedList.push(3);
+
+myLinkedList.unshift(0);
+
+console.log(myLinkedList);
