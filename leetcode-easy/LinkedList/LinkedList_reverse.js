d455c7ca4d54101fa5404a6251ec5ff09b3d947c
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-01-05 03:34:20 -0500

    reverse method

---
+++ leetcode-easy/LinkedList/LinkedList_reverse.js
@@ -0,0 +1,52 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  reverse() {
+    let temp = this.head;
+    this.head = this.tail;
+    this.tail = temp;
+    let prev = null;
+    let next = temp.next;
+    for (let i = 0; i < this.length; i++) {
+      next = temp.next;
+      temp.next = prev;
+      prev = temp;
+      temp = next;
+    }
+    return this;
+  }
+}
+
+const myLinkedList = new LinkedList(1);
+
+myLinkedList.push(2);
+myLinkedList.push(3);
+
+myLinkedList.reverse();
+
+console.log(myLinkedList);
