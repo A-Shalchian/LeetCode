c8ee8c251c2656c9bf88e996dbc12b061aae6b4a
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-01-15 17:26:30 -0500

    initial commit

---
+++ LeetCode Tutorials/Trees/BinarySearchTree.js
@@ -0,0 +1,17 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.right = null;
+    this.left = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+}
+
+let myTree = new BST();
+
+console.log(myTree);
