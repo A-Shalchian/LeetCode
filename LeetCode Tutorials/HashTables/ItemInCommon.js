eaadef80a2827699add113ae9e60810160982b31
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-01-17 13:34:35 -0500

    implemented o(n) method

--- LeetCode Tutorials/HashTables/ItemInCommon.js
+++ LeetCode Tutorials/HashTables/ItemInCommon.js
@@ -1,13 +1,24 @@
 function itemInCommon(arr1, arr2) {
+  // O(n^2)
+  //   for (let i = 0; i < arr1.length; i++) {
+  //     for (let j = 0; j < arr2.length; j++) {
+  //       if (arr1[i] === arr2[j]) return true;
+  //     }
+  //   }
+  //   return false;
+
+  // O(2n) = O(n)
+  let obj = {};
   for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr2.length; j++) {
-      if (arr1[i] === arr2[j]) return true;
-    }
+    obj[arr1[i]] = true;
+  }
+  for (let j = 0; j < arr2.length; j++) {
+    if (obj[arr2[j]]) return true;
   }
   return false;
 }
 
 let arr1 = [1, 3, 5];
-let arr2 = [2, 4, 6];
+let arr2 = [2, 4, 5];
 
 console.log(itemInCommon(arr1, arr2));
