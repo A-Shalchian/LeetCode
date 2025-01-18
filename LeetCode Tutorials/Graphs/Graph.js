1cb1d1b8db14bf10dddb9b04a2f7381c1bdc2037
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-01-18 16:39:09 -0500

    implemented removeVertex method

--- LeetCode Tutorials/Graphs/Graph.js
+++ LeetCode Tutorials/Graphs/Graph.js
@@ -33,6 +33,16 @@ class Graph {
     }
     return false;
   }
+
+  removeVertex(vertex) {
+    if (!this.adjacencyList[vertex]) return undefined;
+    while (this.adjacencyList[vertex].length) {
+      let temp = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, temp);
+    }
+    delete this.adjacencyList[vertex];
+    return this;
+  }
 }
 
 let myGraph = new Graph();
