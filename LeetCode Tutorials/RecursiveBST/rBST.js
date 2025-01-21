ee8aec322440bcc52b48a04379cc7f8fe2114ded
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-01-21 08:35:24 -0500

    implemented recursive contain method for BST

---
+++ LeetCode Tutorials/RecursiveBST/rBST.js
@@ -0,0 +1,57 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.right = null;
+    this.left = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let temp = this.root;
+    while (true) {
+      if (newNode.value === temp.value) return undefined;
+      if (newNode.value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        }
+        temp = temp.left;
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        }
+        temp = temp.right;
+      }
+    }
+  }
+
+  rContains(value, currentNode = this.root) {
+    if (currentNode === null) return false;
+
+    if (value === currentNode.value) return true;
+
+    if (value < currentNode.value) {
+      return this.rContains(value, currentNode.left);
+    } else {
+      return this.rContains(value, currentNode.right);
+    }
+  }
+}
+
+let myBST = new BST();
+
+myBST.insert(47);
+myBST.insert(27);
+
+console.log(myBST.rContains(27));
