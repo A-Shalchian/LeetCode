83b6592f905f6abeb5857e10b6173fadb4b5dd0f
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-01-26 19:35:23 -0500

    insertion sort implemented

---
+++ LeetCode Tutorials/Sorts/BasicSorts/InsertionSort.js
@@ -0,0 +1,14 @@
+function insertonSort(array) {
+  let temp;
+  for (let i = 1; i < array.length; i++) {
+    temp = array[i];
+    for (var j = i - 1; array[j] > temp && j > -1; j--) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = temp;
+  }
+  return array;
+}
+
+myarr = [1, 2, 4, 3, 5, 6];
+console.log(insertonSort(myarr));
