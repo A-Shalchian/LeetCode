a8551a55ece2db49250928b205928ca8c819df35
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-02-06 15:27:43 -0500

    add two numbers question added

---
+++ 1-50/2-addTwoNumbers/addTwoNums.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+var addTwoNumbers = function (l1, l2) {
+  let dummy = new ListNode(0);
+  let current = dummy;
+  let carry = 0;
+
+  while (l1 || l2) {
+    let val1 = l1 ? l1.val : 0;
+    let val2 = l2 ? l2.val : 0;
+
+    let sum = val1 + val2 + carry;
+    carry = Math.floor(sum / 10);
+
+    current.next = new ListNode(sum % 10);
+    current = current.next;
+
+    if (l1) l1 = l1.next;
+    if (l2) l2 = l2.next;
+  }
+
+  if (carry) {
+    current.next = new ListNode(carry);
+  }
+  return dummy.next;
+};
