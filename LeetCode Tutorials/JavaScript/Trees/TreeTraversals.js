7952c9b6b9ff5783d7e3ac70753bfaceddd640a1
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-02-08 18:13:10 -0500

    added DFS PreOrder

--- LeetCode Tutorials/JavaScript/Trees/TreeTraversals.js
+++ LeetCode Tutorials/JavaScript/Trees/TreeTraversals.js
@@ -73,6 +73,18 @@ class BST {
     }
     return results;
   }
+
+  DFSPreOrder() {
+    let results = [];
+
+    function traverse(currentNode) {
+      results.push(currentNode.value);
+      if (currentNode.left) traverse(currentNode.left);
+      if (currentNode.right) traverse(currentNode.right);
+    }
+    traverse(this.root);
+    return results;
+  }
 }
 
 let myTree = new BST();
