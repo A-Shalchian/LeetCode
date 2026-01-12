d43cae92d87879ac1ef76d93725d2434fbfb3b5d
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2026-01-12 18:09:39 -0500

    solution added

---
+++ Python/13- Roman to Integer/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        chars = {'I': 1, "V": 5, "X": 10, "L":50, "C":100, "D":500,"M":1000}
+
+        sum = 0
+
+        for i in range(len(s)):
+            if i + 1 < len(s) and chars[s[i]] < chars[s[i+1]]:
+                sum -= chars[s[i]]
+            else:
+                sum += chars[s[i]]
+
+        return sum
